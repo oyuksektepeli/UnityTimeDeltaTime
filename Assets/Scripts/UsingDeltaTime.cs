f21c110ef70d4606b2392ca3b531752116f63ee2
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-11-12 15:11:59 +0300

    Initial commit

---
+++ Assets/Scripts/UsingDeltaTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class UsingDeltaTime : MonoBehaviour
+{
+    public float speed = 8f;
+    public float countdown = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0.0f)
+        {
+            GetComponent<Light>().enabled = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+    }
+}
